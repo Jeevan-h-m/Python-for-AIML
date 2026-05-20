6952edb44f5905a9535ef1b39c05a80150c9f70b
jeevan-h-m <jeevanhm72@gmail.com>
2026-05-20 18:58:49 +0530

    updated conditions topic

--- conditions.py
+++ conditions.py
@@ -1,8 +1,20 @@
-x=input("enter x value:")
-y=input("enter y value:")
-if x>y:
-    print("x is greater than y")
-elif x<y:
-    print("x is less than y")
+# #
+# x=input("enter x value:")
+# y=input("enter y value:")
+# if x>y:
+#     print("x is greater than y")
+# elif x<y:
+#     print("x is less than y")
+# else:
+#     print("x is equal to y")
+
+
+age=int(input("enetr your age:"))
+if age>=18:
+    print("eligible to vote")
 else:
-    print("x is equal to y")
+    print("not eligible to vote")
+
+
+
+    
